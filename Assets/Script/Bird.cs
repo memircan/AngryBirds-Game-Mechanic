5a877a1009dc5e8ae77b0b3ff84fde7493b0f489
Emircan Tamer <e.tamer_@hotmail.com>
2024-08-11 00:23:25 +0300

    update

--- Assets/Script/Bird.cs
+++ Assets/Script/Bird.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Ball : MonoBehaviour
+public class Bird : MonoBehaviour
 {
 
     public Rigidbody2D rb;
